9af69e3ae63417cbdcc8eacb7515b8c429b738ac
froux <froux@frouxs-MacBook-Pro.local>
2019-06-14 16:45:07 +0200

    Ejercicios Semana 4 ICSI

--- IntroALosMotoresGraficos-908_INGCO902_502050_X01/Ejercicios/semana3B/ejercicio3B-scene-graph.js
+++ IntroALosMotoresGraficos-908_INGCO902_502050_X01/Ejercicios/semana3B/ejercicio3B-scene-graph.js
@@ -4,31 +4,33 @@ var camera, scene, renderer;
 var cameraControls;
 var clock = new THREE.Clock();
 var ambientLight, light;
+
 // TODO declarar los nodos a los que aplicar las rotaciones
-/*
-var nodo_cabeza;
-var nodo_parte_superior;
-var nodo_cuello_para_arriba;
-*/
+var nodo_cabeza = new THREE.Object3D();
+var nodo_cuello_para_arriba = new THREE.Object3D();
+var nodo_parte_superior = new THREE.Object3D();
 
 function negar_obj()
 {
 var val = controlValores.cuello;
 // TODO asignar la rotacion adecuada al nodo adecuado, y... control de limites?
-// nodo_adecuado.rotation.x = val;
+if (val <= 0.1 & val >= -0.1){
+	nodo_cuello_para_arriba.rotation.x = val;
+};
+
 
 }
 function inclina_obj(val)
 {
 var val = controlValores.cintura;
 // TODO asignar la rotacion adecuada al nodo adecuado, y... control de limites?
-// nodo_adecuado.rotation.x = val;
+nodo_parte_superior.rotation.z = val;
 }
 function mirar_lado(val)
 {
 var val = controlValores.cabeza;
 // TODO asignar la rotacion adecuada al nodo adecuado, y... control de limites?
-// nodo_adecuado.rotation.x = val;
+nodo_cabeza.rotation.y = val;
 }
 
 function setupGUI()
@@ -88,12 +90,12 @@ function init() {
 	// OBJECTS
 	
     
-        var migeometria = new THREE.SphereGeometry( 0.5, 64, 64 );//Geometry();
+    var migeometria = new THREE.SphereGeometry( 0.5, 64, 64 );//Geometry();
 	var torsogeometria = new THREE.CylinderGeometry( 0.6, 0.1, 1, 32 );
 	var narizgeometria = new THREE.CylinderGeometry( 0.2, 0.05, 0.5, 32 );
 	var cuellogeometria = new THREE.BoxGeometry( 0.1, 0.5, 0.1 );//CylinderGeometry( 0.1, 0.1, 0.5, 32 );
+	//narizgeometria.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0.5/2, 0) ); 
 	
-    
     var material = new THREE.MeshPhongMaterial( { color: 0xFF0000 } ); // Material de color rojo
     var bola1 = new THREE.Mesh (migeometria, material); // Crea la base 
     var torso = new THREE.Mesh (torsogeometria, material); // Crea el cono que representa el torso
@@ -106,40 +108,62 @@ function init() {
 	scene = new THREE.Scene();
 	scene.add( light );
 	scene.add( ambientLight );
-
+	
 	var monigote = new THREE.Object3D();
 	//TODO generar el grafo de escena y asignar los objetos tipo Mesh a los nodos en el grafo de escena 
 	//     con las posiciones y orientaciones adecuadas de forma que permita al monigote:
 	//  1.- inclinarse y levantarse (rotación respecto al eje z: desde cintura para arriba sin modificar la base)
 	//  2.- negar con el cuello y la cabeza (rotación respecto a eje x: de cuello para arriba sin modificar ni base ni torso)
 	//  3.- mirar izquierda/derecha (rotación respecto al eje y: solo rota la cabeza (esfera +nariz), el cuello se queda en su sitio
-	// posicionamiento de los objetos: 
-	//	la base va hasta una altura de 0.75 (desde -0.75 hasta0.75) es 1.5 veces más grande que la esfera definida en "migeometria"
-	//	el tronco hay que situralo justo tocando la base. Teniendo en cuenta su longitud, va desde 0.75 hasta 1.75. Inclinarse debe rotar desde esa altura 
-	//	el cuello hay que situarlo tocando el torso: debe ir de 1.75 a 2.25. La negación debe rotar desde su punto mas bajo.
-	//	la cabeza tendrá que situarse tocando el cuello
-	//	la nariz hay que situarla tocando la cabeza hacia fuera. La cabeza tiene un radio de 0.5 por lo que habrá que desplazar la nariz.
 	
-	// TODO  eliminar esto que es incorrecto
-	scene.add(bola1); 
-	bola1.position.x = 2;
-	// TODO  eliminar esto que es incorrecto
-	scene.add(bola3); 
-	bola3.position.x = -2;
-	// TODO  eliminar esto que es incorrecto
-	scene.add(torso); 
-	torso.position.y=2;
-	// TODO  eliminar esto que es incorrecto
-	scene.add(cuello); 
-	cuello.position.y = -2;
-	// TODO  eliminar esto que es incorrecto
-	scene.add(nariz); 
-	// TODO añadir a la escena el monigote
-	// scene.add( monigote );
+	// posicionamiento de los objetos: 
+	
+	//	la base va hasta una altura de 0.75 (desde -0.75 hasta 0.75) es 1.5 veces más grande que la esfera definida en "migeometria"
+	//scene.add(bola1); 
+	bola1.position.x = 0;
+	bola1.position.y = 0;
+	bola1.scale.x = 1.5;
+	bola1.scale.y = 1.5;
+	bola1.scale.z = 1.5;
+	
+	//	el tronco hay que situralo justo tocando la base. Teniendo en cuenta su longitud, va desde 0.75 hasta 1.75. Inclinarse debe rotar desde esa altura 
+	//scene.add(torso); 
+	torso.position.x = 0;
+	torso.position.y=1.25;
+	
+	//	el cuello hay que situarlo tocando el torso: debe ir de 1.75 a 2.25. La negación debe rotar desde su punto mas bajo.
+	//scene.add(cuello); 
+	cuello.position.y = 2;
+	cuello.position.x = 0;
+	
+	//	la cabeza tendrá que situarse tocando el cuello
+	//scene.add(bola3); 
+	bola3.position.x = 0;
+	bola3.position.y = 2.75;
+	
+	//	la nariz hay que situarla tocando la cabeza hacia fuera. La cabeza tiene un radio de 0.5 por lo que habrá que desplazar la nariz.
+	//scene.add(nariz); 
+	nariz.position.x = 0.5;
+	nariz.position.y = 2.75;
+	nariz.rotation.z = 0.5*Math.PI;
+	
+	// añadir a la escena el monigote	
+	nodo_cabeza.add(bola3);
+	nodo_cabeza.add(nariz);
+	
+	nodo_cuello_para_arriba.add(nodo_cabeza);
+	nodo_cuello_para_arriba.add(cuello);
+	
+	nodo_parte_superior.add(torso);
+	nodo_parte_superior.add(nodo_cuello_para_arriba);
+	
+	monigote.add(nodo_parte_superior);
+	monigote.add(bola1);
+	
+	scene.add( monigote );
+	
 
         setupGUI();
-
-
 }
 
 function animate() {
---
+++ IntroALosMotoresGraficos-908_INGCO902_502050_X01/Ejercicios/semana4A/ejercicio4A-cambio-a-SRC.js
@@ -0,0 +1,366 @@
+// primer ejercicio con three.js
+// crear una geometria teniendo en cuenta el orden de los vértices
+var camera, scene, renderer;
+var camera_v;
+var handcamera;
+var camNew;
+var cameraControls;
+var controlValores;
+var clock = new THREE.Clock();
+var ambientLight, light;
+
+var R = new THREE.Vector3(1,0,0);  // los ejes del sistema de referencia de la cámara
+var U = new THREE.Vector3(0,1,0);  //   coordenadas en el sistema de referencia del mundo
+var D = new THREE.Vector3(0,0,1);  //  Right, Up y Deep
+
+var E = new THREE.Vector3(0,0,2); // posicion camara
+var at = new THREE.Vector3(0,0,0);  // punto al que mira
+var up = new THREE.Vector3(0,0,0);  // vector que indica verticalidad
+
+var ejes; // el objeto de la escena que representa a la cámara, mediante líneas azules
+var pir_lines;  //el objeto que se coloca en frente de la cámara
+
+function mostrar_consola()
+{
+
+// posicion de la nueva camera (1)
+E.x = controlValores.PX;
+E.y=controlValores.PY;
+E.z=controlValores.PZ;
+
+// punto de atencion (2)
+at.x=controlValores.aX;
+at.y=controlValores.aY;
+at.z=controlValores.aZ;
+
+// verticalidad de la camera (3)
+up.x=controlValores.UpX;
+up.y=controlValores.UpY;
+up.z=controlValores.UpZ;
+
+console.log("estoy en = "+E.x+", " + E.y+", " +E.z);//position
+console.log("    mirando a "+at.x+", " +at.y +", " +at.z);//mirando a
+
+// TODO 1 recalcular las coordenadas de los ejes del sistema de referencia de la cámara
+//	partiendo de la posición, punto al que mira y el vector up
+//	dejar las coordenadas en los vectores R, U y D (Right, Up, Deep)
+
+
+//Pasar Sistema referencia local a Sistema referencia mundo
+var D = new THREE.Vector3(E.x-at.x,E.y-at.y,E.z-at.z);// posticion camera - punto donde camera esta mirando
+D.normalize();// vector unitario
+
+var R = new THREE.Vector3();
+R.crossVectors(up,D);
+R.normalize();// vector unitario
+
+var U = new THREE.Vector3();
+U.crossVectors(D,R);// ya es vector unitario
+
+console.log("R = "+R.x+", " + R.y+", " +R.z);
+console.log("U = "+U.x+", " + U.y+", " +U.z);
+console.log("D = "+D.x+", " + D.y+", " +D.z);
+
+// TODO 1: 
+// recalcular las coordenadas del objeto que representa a la cámara en funcion de la posición de la cámara (variable E) y de los tres ejes de su sistema de referencia (variables R, U y D):
+//	ejes.geometry.vertices[i]
+
+//        vertice 0 ---> derecha de la cámara (eje X: posición ćamara + R)
+ejes.geometry.vertices[0].addVectors(E,R);
+//        1 ---> posición de la cámara
+ejes.geometry.vertices[1].copy(E);
+//        2 ---> encima de la cámara (eje Y)
+ejes.geometry.vertices[2].addVectors(E,U);
+//        3 ---> posición de la cámara
+ejes.geometry.vertices[3].copy(E);
+//        4 ---> en el eje Z de la cámara
+ejes.geometry.vertices[4].addVectors(E,D);
+//        5 ---> posición de la cámara
+ejes.geometry.vertices[5].copy(E);
+//        6 ---> delante de la cámara, y a la derecha (posición cámara - D + R)
+ejes.geometry.vertices[6].set( E.x-D.x+R.x, E.y-D.y+R.y, E.z-D.z+R.z);
+//        7 ---> delante de la cámara, y a la izquierda
+ejes.geometry.vertices[7].set( E.x-D.x-R.x, E.y-D.y-R.y, E.z-D.z-R.z);
+//        8 ---> posición de la cámara
+ejes.geometry.vertices[8].copy(E);
+/********/
+
+
+
+/********/
+ejes.geometry.verticesNeedUpdate=true;
+
+
+
+// TODO 2: asignar a la cámara nueva camNew:
+//	1.- la posición de la cámara
+//	2.- el vector up
+//	3.- La matriz de cambio de sistema de referencia mediante 
+//	    el método lookAt asociado a las cámaras.
+/***************/
+camNew.position.copy(E);
+camNew.up.copy( up );
+camNew.lookAt( at );
+camNew.updateMatrixWorld ( true );
+/****************/
+
+
+// TODO 3: asignar directamente las matrices a la cámara nueva camNew (set o copy o lo que sea)
+//	pero para ello hay que comentar el código que se ha añadido para el ejercicio 2
+// las matrices que hay que asignar son:
+// 1.- camNew.matrix.  
+// 2.- camNew.matrixWorld	Matriz que posiciona la cámara en el 
+//				mundo. Estas dos matrices, en nuestro caso, son iguales,
+//				y representan el cambio de sistema de referencia de
+//				la cámara al sistema de referencia del mundo (local --> mundo)
+// 3.- camNew.matrixWorldInverse   Matriz que pasa al sistema de referencia de la cámara
+/**********/
+
+// posicion (E)
+// punto de atencion (at)
+// verticalidad (up)
+
+var W2L = new THREE.Matrix4(); // del mundo a la camera
+W2L.set(
+	R.x, 	R.y, 	R.z, 	-R.dot(E), 
+	U.x, 	U.y, 	U.z, 	-U.dot(E),
+	D.x, 	D.y, 	D.z, 	-D.dot(E),
+	0,	0,	0,	1
+);
+
+var L2W = new THREE.Matrix4(); // de la camera al mundo
+L2W.set(
+	R.x, 	U.x, 	D.x, 	0, 
+	R.y, 	U.y, 	D.y, 	0,
+	R.z, 	U.z, 	D.z, 	0,
+	0,	0,	0,	1	
+	);
+
+camNew.matrix.copy( L2W );
+camNew.matrixWorld.copy( L2W );
+camNew.matrixWorldInverse.copy( W2L );
+camNew.updateMatrixWorld ( true );
+
+/***********/
+
+
+// TODO 3 hacer que el objeto pir_lines se mueva junto con la cámara, 
+//     pero sin modificar las coordenadas de los vértices, modificando su matriz.
+//     igual que en el TODO 2 para la función relolocar_pir.
+//     Las matrices a modificar son: pir_lines.matrix y pir_lines.matrixWorld
+/***************/
+
+pir_lines.matrix.copy( camNew.matrixWorld );
+pir_lines.matrixWorld.copy( camNew.matrixWorld );
+
+/**************/
+
+}
+
+
+
+function setupGUI()
+{
+
+controlValores =
+    {
+    PX: 0.0,
+    PY: 0.0,
+    PZ: 2.0,
+    aX: 0.0,
+    aY: 0.0,
+    aZ: 0.0,
+    UpX: 0.0,
+    UpY: 1.0,
+    UpZ: 0.0
+    };
+
+var gui = new dat.GUI();
+
+// parametros para obtener los ejes del sistema de referencia de la cámara
+
+// position de la camera
+gui.add( controlValores, "PX", -5, 5 ).name( "P_X" ).onChange( mostrar_consola );
+gui.add( controlValores, "PY", -5, 5 ).name( "P_Y" ).onChange( mostrar_consola );
+gui.add( controlValores, "PZ", -5, 5 ).name( "P_Z" ).onChange( mostrar_consola );
+
+// punto a quel esta mirando la camera
+gui.add( controlValores, "aX", -5, 5 ).name( "aX" ).onChange( mostrar_consola );
+gui.add( controlValores, "aY", -5, 5 ).name( "aY" ).onChange( mostrar_consola );
+gui.add( controlValores, "aZ", -5, 5 ).name( "aZ" ).onChange( mostrar_consola );
+
+// indica la verticalidad de la camera
+gui.add( controlValores, "UpX", -1, 1 ).name( "UpX" ).onChange( mostrar_consola );
+gui.add( controlValores, "UpY", -1, 1 ).name( "UpY" ).onChange( mostrar_consola );
+gui.add( controlValores, "UpZ", -1, 1 ).name( "UpZ" ).onChange( mostrar_consola );
+}
+
+function cam(val)
+{
+if (val === 1) camera_v = camera;
+    else camera_v = camNew; 
+}
+
+function recolocar_pir()
+{
+// TODO 2: posicionar el objeto pir_lines frente a la camara nueva
+//         Hay que modificar las matrices de pir_lines no las coordenadas de los vértices
+//         La pregunta a responder es: Donde pueden estar esas matrices?
+//         Las matrices a modificar son: pir_lines.matrix y pir_lines.matrixWorld
+/***********/
+
+
+pir_lines.matrix.copy( camNew.matrixWorld );
+pir_lines.matrixWorld.copy( camNew.matrixWorld );
+
+
+/************/
+}
+
+function init() {
+	var canvasWidth = window.innerWidth * 0.9;
+	var canvasHeight = window.innerHeight * 0.9;
+
+// TODO 2: hay que descomentar todo este bloque para que aparezcan los botones
+/*************/
+	container = document.createElement('div');
+	document.body.appendChild(container);
+
+	var info = document.createElement('div');
+	info.style.position = 'absolute';
+	info.style.top = '10px';
+	info.style.width = '100%';
+	info.style.textAlign = 'center';
+	info.innerHTML += 'cambio de cámara:<input id="cam1" type="button" onclick="cam(1)" value="cam1"/><input id="cam_new" type="button" onclick="cam(2)" value="cam_new"/><input id="coloca_pir" type="button" onclick="recolocar_pir()" value="colocar pantalla"/>';
+	container.appendChild(info);
+/******************** hasta aqui ********/
+
+	
+	// CAMERA
+
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 80000 );
+	camera.position.set(-1,1,7);
+	camera.lookAt(0,0,0);
+
+
+
+	camNew = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 80000 );
+	camNew.position.set(0,0,2);
+	camNew.lookAt(0,0,0);
+	// LIGHTS
+
+	light = new THREE.DirectionalLight( 0xFFFFFF, 0.7 );
+	light.position.set( 4, 4, 4 );
+	light.target.position.set(0, 0, 0);
+	light.target.updateMatrixWorld()
+
+	var ambientLight = new THREE.AmbientLight( 0x111111 );
+
+	// RENDERER
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( canvasWidth, canvasHeight );
+	renderer.setClearColor( 0xAAAAAA, 1.0 );
+
+	renderer.gammaInput = true;
+	renderer.gammaOutput = true;
+
+	// Add to DOM
+	var container = document.getElementById('container');
+	container.appendChild( renderer.domElement );
+
+	// CONTROLS
+	cameraControls = new THREE.OrbitControls( camera, renderer.domElement );
+	cameraControls.target.set(0, 0, 0);
+
+	// OBJECT
+	// ejes: las líneas que representan el sistema de Ref de la cámara
+	var line_material = new THREE.LineBasicMaterial({color: 0x0000ff});
+
+	XYZgeometry = new THREE.Geometry();
+	XYZgeometry.vertices.push(
+	new THREE.Vector3( 1, 0, 2 ), // posicion_camara+R
+	new THREE.Vector3( 0, 0, 2 ), // posición_camara
+	new THREE.Vector3( 0, 1, 2 ), // posicion_camara+U
+	new THREE.Vector3( 0, 0, 2 ), // posición_camara
+	new THREE.Vector3( 0, 0, 3 ), // posicion_camara+D
+	new THREE.Vector3( 0, 0, 2 ), // posición_camara
+	new THREE.Vector3( 1, 0, 1 ), // posicion_camara-D+R
+	new THREE.Vector3( -1, 0, 1 ), // posicion_camara-D-R
+	new THREE.Vector3( 0, 0, 2 ) // posición_camara
+);
+	ejes = new THREE.Line( XYZgeometry, line_material );
+
+		// ejes del Sistema de Ref del mundo
+	var M_line_material = new THREE.LineBasicMaterial({color: 0xff0000});
+
+	var M_XYZgeometry = new THREE.Geometry();
+	M_XYZgeometry.vertices.push(
+	new THREE.Vector3( 1, 0, 0 ), // X
+	new THREE.Vector3( 0, 0, 0 ), // O
+	new THREE.Vector3( 0, 1, 0 ), // Y
+	new THREE.Vector3( 0, 0, 0 ), // 0
+	new THREE.Vector3( 0, 0, 1 ) // Z
+);
+	var M_ejes = new THREE.Line( M_XYZgeometry, M_line_material );
+
+/*********************/
+	var pirgeometry = new THREE.Geometry();
+	pirgeometry.vertices.push(
+	new THREE.Vector3( 0, 0, 0 ), // 
+	new THREE.Vector3( 0.6, 0.5, -0.6 ), //   dcha, arriba, delante
+	new THREE.Vector3( -0.6, 0.5, -0.6 ), //  izda arriba, delante
+	new THREE.Vector3( -0.6, -0.5, -0.6 ), //  izda, abajo, delante
+	new THREE.Vector3( 0.6, -0.5, -0.6 ), //  dcha, abajo, delante
+	new THREE.Vector3( 0.6, 0.5, -0.6 ) //  dcha, arriba, delante
+	);
+	pir_lines = new THREE.Line( pirgeometry, M_line_material );
+	
+	pir_lines.matrixAutoUpdate=false;       // no queremos que se recalculen sus 
+					//matrices en funcion de rotation, scale y position
+	pir_lines.rotationAutoUpdate=false;	// modificaremos sus matrices a mano
+/***********************/
+	
+	var material = new THREE.MeshPhongMaterial( { color: 0xFff000 } ); // Material esfera
+	var miotrageometria= new THREE.SphereGeometry( 0.4, 64, 64 ); //BoxGeometry( 1, 1, 1 );
+	var miotroobjeto = new THREE.Mesh (miotrageometria, material); 
+
+	var yotroobjeto = new THREE.Mesh (miotrageometria, material); 
+	// SCENE
+
+	scene = new THREE.Scene();
+	scene.add( light );
+	scene.add( ambientLight );
+	scene.add( ejes );
+	scene.add( M_ejes );
+	scene.add( pir_lines );
+
+	scene.add( miotroobjeto );
+	scene.add( yotroobjeto );
+	yotroobjeto.position.z = -2;
+
+        setupGUI();
+	mostrar_consola();
+	camera_v = camera;
+	// TODO 3: descomentar esta sentencia
+	//camNew.matrixAutoUpdate=false;  // calcularemos las matrices nosotros
+
+}
+
+function animate() {
+	window.requestAnimationFrame( animate );
+	render();
+}
+
+function render() {
+	var delta = clock.getDelta();
+	cameraControls.update(delta);
+	renderer.render( scene, camera_v );
+}
+
+try {
+	init();
+	animate();
+} catch(e) {
+	var errorReport = "Your program encountered an unrecoverable error, can not draw on canvas. Error was:<br/><br/>";
+	$('#container').append(errorReport+e);
+}
